f205a7e7b395b755774b52ebcfbac4249b179d89
Ushasri Pathi <ushasrip015@gmail.com>
2023-11-06 22:55:20 +0530

    Program1 code to convert kg to

---
+++ src/main/scala/Program1.scala
@@ -0,0 +1,14 @@
+//program to convert kg to g
+object Program1 {
+
+  def main(args: Array[String]): Unit = {
+
+    var kg = 56
+
+    var g = kg * 1000;
+
+    println("Output of given 56 kgs in grams is " +g)
+
+  }
+
+}
