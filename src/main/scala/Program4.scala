b4cc5f1ce6262a134e03dc9078c6915c57440ff9
Ushasri Pathi <ushasrip015@gmail.com>
2023-11-06 23:47:59 +0530

    Program4 code to declare a number and divide it with 3 and 2 based on even and odd number

---
+++ src/main/scala/Program4.scala
@@ -0,0 +1,26 @@
+//Check even or odd
+object Program4 {
+
+  def main(args: Array[String]):Unit = {
+
+    val num = 299
+
+    //checking whether the range falls between 100-100
+
+    if(num >100 && num <1000){
+
+      //check whether the number is even or odd
+      if(num%2 == 0){
+        var evenremainder = num/3
+        println(s"remainder is $evenremainder")
+      }else{
+        println(s"the number $num is Odd number")
+        var oddremainder = num/2
+        println(s"the remainder is $oddremainder")
+      }
+    }else{
+      println("The entered number is wrong number")
+    }
+  }
+
+}
