b4cb226db16f2e3b9f81e215b8c2f8bf3acea749
Ushasri Pathi <ushasrip015@gmail.com>
2023-11-07 13:54:03 +0530

    Program9 to sum all the numbers from 56 to 153

---
+++ src/main/scala/Program9.scala
@@ -0,0 +1,13 @@
+object Program9 {
+
+  def main(args: Array[String]): Unit ={
+    var sum = 0
+
+    for(num<- 56 to 153){
+      sum += num
+    }
+    println(s"Sum all the numbers from 56 to 153 is: $sum")
+
+  }
+
+}
